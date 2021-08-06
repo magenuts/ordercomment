c8d01b551d254c007ce90cae41cae47e0e3ca7fb
Benjamin Frade <ijaz.ali.sse@gmail.com>
2021-08-06 17:27:12 +0500

    add core module dependency and remove bug.

--- Model/CustomBlockConfigProvider.php
+++ Model/CustomBlockConfigProvider.php
@@ -2,7 +2,7 @@
 /**
  * @category Magenuts FrontOrderComment
  * @package Magenuts_FrontOrderComment
- * @copyright Copyright (c) 2017 Magenuts
+ * @copyright Copyright (c) 2017-2021 Magenuts
  * @author Magenuts Team <support@magenuts.com>
  */
 namespace Magenuts\FrontOrderComment\Model;
